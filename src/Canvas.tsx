3935bcad81695cd46eeb636a8734bda864142fb0
Chris Chalstrom <chris.chalstrom@gmail.com>
2019-02-09 16:03:27 -0700

    set canvas dimensions from image size

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { SFC, useRef, useEffect } from 'react';
+import React, { SFC, useRef, useEffect, useState } from 'react';
 
 import './Canvas.css';
 
@@ -8,7 +8,10 @@ interface CanvasProps {
 
 export const Canvas: SFC<CanvasProps> = (props) => {
   const { initialImage } = props;
+
   const canvas = useRef(null as HTMLCanvasElement | null);
+  const imageElement = useRef(null as HTMLImageElement | null);
+  const [{ width, height }, setDimensions] = useState({ width: 300, height: 200 });
 
   useEffect(() => {
     if (!initialImage || !canvas.current) { return; }
@@ -23,8 +26,12 @@ export const Canvas: SFC<CanvasProps> = (props) => {
       image.src = reader.result as string;
 
       image.onload = () => {
+        imageElement.current = image;
+
+        setDimensions({ width: image.width, height: image.height });
         const ctx = (canvas.current as HTMLCanvasElement).getContext('2d');
         (ctx as CanvasRenderingContext2D).drawImage(image, 0, 0);
+
       }
     };
 
@@ -34,8 +41,8 @@ export const Canvas: SFC<CanvasProps> = (props) => {
   return (
     <div className="canvas-container">
       <canvas
-        width={800}
-        height={800}
+        width={width}
+        height={height}
         ref={canvas}
       />
     </div>
